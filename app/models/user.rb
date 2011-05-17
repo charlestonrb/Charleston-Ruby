208d7bcb73f81b837fe07fcb6a117331bccfd5dd
gradus <kriswindham@gmail.com>
2011-05-16 21:15:09 -0400

    bcrypt salt and hash

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,7 +20,8 @@ class User < ActiveRecord::Base
 
   def encrypt_password
     if password.present?
-      BCrypt::Password.create(password)
+      self.password_salt = BCrypt::Engine.generate_salt
+      self.password_hash = BCrypt::Engine.hash_secret(password, password_salt)
     end
   end
 end
