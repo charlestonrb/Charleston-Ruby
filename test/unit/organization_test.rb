f0a0f701cf41132cb65a00524108df4c90f4f063
gradus <kriswindham@gmail.com>
2011-06-15 19:39:18 -0400

    remove leftover user model controller helper and tests

--- test/unit/organization_test.rb
+++ test/unit/organization_test.rb
@@ -4,7 +4,4 @@ class OrganizationTest < ActiveSupport::TestCase
   test 'organization' do
     assert_equal Organization.new.valid?, true
   end
-  # test "the truth" do
-  #   assert true
-  # end
 end
